55e5507570c8d19fbec42424429f6be143886862
ZULVAN AVITO <shawnanggara@gmail.com>
2023-12-16 18:17:30 +0800

    first massage

---
+++ app/src/main/java/com/bangkit/braitexample/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.braitexample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.braitexample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
---
+++ app/src/main/java/com/bangkit/braitexample/ui/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.bangkit.braitexample.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.braitexample.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_activity)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DELAY_TIME)
+    }
+
+    companion object {
+        const val DELAY_TIME = 1200L
+    }
+}
